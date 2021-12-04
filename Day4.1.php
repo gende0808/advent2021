255bb90e11b8222eb988e3668ae2b599760dc0d0
Gregory <gende@novacollege.nl>
2021-12-04 17:37:56 +0100

    part 1 beautified a little

--- Day4.1.php
+++ Day4.1.php
@@ -5,42 +5,21 @@ $listOfCards = array();
 for ($i = 2; $i < count($input); $i += 6) {
     $bingoCard = array();
     for ($j = $i; $j < $i + 5; $j++) {
-        $bingonums = preg_split('/\s+/', $input[$j]);
-        if (count($bingonums) == 6) {
-            unset($bingonums[0]);
-            $bingonums = array_values($bingonums);
-        }
-        array_push($bingoCard, $bingonums);
+        $bingonums = array_push($bingoCard, preg_split('/\s+/', trim($input[$j])));
     }
     array_push($listOfCards, $bingoCard);
 }
 foreach ($drawnNumbers as $drawnNumber) {
+    array_walk_recursive($listOfCards, function (&$value) {
+        global $drawnNumber;
+        $value = ($value == $drawnNumber) ? 0 : $value;
+    });
     foreach ($listOfCards as $key => $card) {
         foreach ($card as $keyRow => $row) {
-            foreach ($row as $keynum => $num) {
-                if ($num == $drawnNumber) {
-                    $listOfCards[$key][$keyRow][$keynum] = 0;
-                }
+            if (array_sum($row) == 0 || array_sum((array_column($card, $keyRow))) == 0) {
+                echo array_sum((call_user_func_array('array_merge', $card))) * $drawnNumber;
+                exit;
             }
         }
     }
-    foreach ($listOfCards as $key => $card) {
-        foreach ($card as $keyRow => $row) {
-            if (array_sum($row) == 0) {
-                printCard($card, $drawnNumber);
-            }
-            if (array_sum((array_column($card, $keyRow))) == 0) {
-                printCard($card, $drawnNumber);
-            }
-        }
-    }
-}
-function printCard($card, $drawnNumber)
-{
-    $total = 0;
-    foreach ($card as $row) {
-        $total += array_sum($row);
-    }
-    echo $total * $drawnNumber;
-    exit();
 }
