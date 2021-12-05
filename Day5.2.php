d527e373b726f4daee64067db6ad74af89136d8b
Gregory <gende@novacollege.nl>
2021-12-05 22:31:01 +0100

    MOTHERFUCKING SPACESHIP OPERATOR BABY

--- Day5.2.php
+++ Day5.2.php
@@ -9,32 +9,12 @@ foreach ($input as $line) {
 }
 $map = array();
 foreach ($parsed as $coordinates) {
-    $ystart = min([$coordinates[1], $coordinates[3]]);
-    $yend = max([$coordinates[1], $coordinates[3]]);
-    $xstart = min([$coordinates[0], $coordinates[2]]);
-    $xend = max([$coordinates[0], $coordinates[2]]);
-    if ($xstart == $xend) {
-        for ($i = $ystart; $i <= $yend; $i++) {
-            $map[$coordinates[0]][$i] += 1;
-        }
-    } elseif ($ystart == $yend) {
-        for ($i = $xstart; $i <= $xend; $i++) {
-            $map[$i][$coordinates[1]] += 1;
-
-        }
-    } else {
-        for($i = $xstart; $i <= $xend; $i++){
+    {
+        $steps = max(abs($coordinates[2] - $coordinates[0]), (abs($coordinates[3] - $coordinates[1])));
+        for($i = 0; $i <= $steps; $i++){
             $map[$coordinates[0]][$coordinates[1]] += 1;
-            if($coordinates[0] < $coordinates[2]){
-                $coordinates[0]++;
-            } else{
-                $coordinates[0]--;
-            }
-            if($coordinates[1] < $coordinates[3]){
-                $coordinates[1]++;
-            } else{
-                $coordinates[1]--;
-            }
+            $coordinates[0] += $coordinates[2] <=> $coordinates[0];
+            $coordinates[1] += $coordinates[3] <=> $coordinates[1];
         }
     }
 }
