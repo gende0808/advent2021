ac83a848131dc70869ec291d690d37bd8cbca8fc
gregory van den Ende <gende@novacollege.nl>
2021-12-06 09:57:51 +0100

    day 6 done

---
+++ Day6.php
@@ -0,0 +1,19 @@
+<?php
+error_reporting(0);
+$input = explode(",", file_get_contents("Day6.txt"));
+foreach ($input as $num) {
+    $fish[$num] += 1;
+}
+for ($i = 0; $i < 256; $i++) {
+    $newState = array();
+    foreach ($fish as $key => $amount) {
+        if ($key == 0) {
+            $newState[6] += $amount;
+            $newState[8] += $amount;
+        } else {
+            $newState[$key - 1] += $amount;
+        }
+    }
+    $fish = $newState;
+}
+echo array_sum($fish);
