c7a6b7bdc90559cdf62d2026c3a4e2ee65f2122a
gregory van den Ende <gende@novacollege.nl>
2021-12-06 11:10:22 +0100

    changed part 1 to use part 2 code

--- Day5.1.php
+++ Day5.1.php
@@ -1,6 +1,5 @@
 <?php
 error_reporting(0);
-include "functions.php";
 $input = explode(PHP_EOL, file_get_contents("Day5.txt"));
 $parsed = array();
 $amount = 0;
@@ -9,17 +8,14 @@ foreach ($input as $line) {
 }
 $map = array();
 foreach ($parsed as $coordinates) {
-    $ystart = min([$coordinates[1], $coordinates[3]]);
-    $yend = max([$coordinates[1], $coordinates[3]]);
-    $xstart = min([$coordinates[0], $coordinates[2]]);
-    $xend = max([$coordinates[0], $coordinates[2]]);
-    if ($xstart == $xend) {
-        for ($i = $ystart; $i <= $yend; $i++) {
-            $map[$coordinates[0]][$i] += 1;
-        }
-    } elseif ($ystart == $yend) {
-        for ($i = $xstart; $i <= $xend; $i++) {
-            $map[$i][$coordinates[1]] += 1;
+    {
+        $steps = max(abs($coordinates[2] - $coordinates[0]), (abs($coordinates[3] - $coordinates[1])));
+        for ($i = 0; $i <= $steps; $i++) {
+            if ($coordinates[0] == $coordinates[2] || $coordinates[1] == $coordinates[3]) {
+                $map[$coordinates[0]][$coordinates[1]] += 1;
+                $coordinates[0] += $coordinates[2] <=> $coordinates[0];
+                $coordinates[1] += $coordinates[3] <=> $coordinates[1];
+            }
         }
     }
 }
