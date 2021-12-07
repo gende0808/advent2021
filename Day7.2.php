6f49ab618a5bae8e68d9190f9e00223732882ec6
gregory van den Ende <gende@novacollege.nl>
2021-12-07 14:39:09 +0100

    part 1 done, bruteforce part 2 later
    ... or not

--- Day7.2.php
+++ Day7.2.php
@@ -1,8 +1,7 @@
 <?php
 $input = explode(",", file_get_contents("Day7.txt"));
-$mean = array_sum($input) / count($input);
+$mean = floor(array_sum($input) / count($input));
 foreach ($input as $num) {
    $result += (abs($num - $mean) * (abs($num - $mean) + 1) / 2);
 }
-//94862005 too low
 echo round($result);
