29a12427d9b78d20c85be326571434bf880cb3cc
Gregory <gende@novacollege.nl>
2021-12-13 00:39:59 +0100

    part 1 done, oof.

--- Day12.1.php
+++ Day12.1.php
@@ -1,51 +1,36 @@
 <?php
-
 include "functions.php";
 $input = explode(PHP_EOL, file_get_contents("Day12.txt"));
 error_reporting(0);
-$totalarray = loop(["start" => 0], ["start"]);
-print_array($totalarray);
-function loop($currentnodes, $previousnodes)
+$totalarray = loop(["start"]);
+echo count($totalarray);
+function loop($strings)
 {
     global $input;
-    $totalArray = array();
-    foreach ($currentnodes as $nodekey => $currentnode) {
-        foreach ($input as $line) {
-            if (str_contains($line, $nodekey)) {
-                $connectednode = str_replace(["-$nodekey", "$nodekey-"], "", $line);
-                if (!in_array($connectednode, $previousnodes) || preg_match('/[A-Z]/', $connectednode[0])) {
-                    $connectednodes[$connectednode] = 1;
-                    $newarray = $previousnodes;
-                    array_push($newarray, $nodekey);
-                    print_array($connectednodes);
-                    $deep = loop2($connectednodes, $newarray);
-                    array_push($connectednodes, $deep);
+    $newStrings = array();
+    foreach ($strings as $string) {
+        $stringArray = explode(" ", $string);
+        $lastConnection = end($stringArray);
+        if ($lastConnection == "end") {
+            array_push($newStrings, $string);
+        } else {
+            foreach ($input as $line) {
+                if (str_contains($line, $lastConnection) && $lastConnection != "end") {
+                    $newlastconnection = str_replace(["-$lastConnection", "$lastConnection-"], "", $line);
+                    end($stringArray);
+                    $previousNode = prev($stringArray);
+                    if (!in_array($newlastconnection, $stringArray) || preg_match('/[A-Z]/', $newlastconnection)) {
+                        $newstring = $string . " " . $newlastconnection;
+                        array_push($newStrings, $newstring);
+
+                    }
                 }
             }
         }
-        array_push($totalArray, $connectednodes);
     }
-    return $totalArray;
-}
-function loop2($currentnodes, $previousnodes)
-{
-    global $input;
-    $totalArray = array();
-    foreach ($currentnodes as $nodekey => $currentnode) {
-        echo $nodekey;
-        foreach ($input as $line) {
-            if (str_contains($line, $nodekey)) {
-                $connectednode = str_replace(["-$nodekey", "$nodekey-"], "", $line);
-                if (!in_array($connectednode, $previousnodes) || preg_match('/[A-Z]/', $connectednode[0])) {
-                    $connectednodes[$connectednode] = 1;
-                    $newarray = $previousnodes;
-                    array_push($newarray, $nodekey);
-//                    $deep = loop2($connectednodes, $newarray);
-//                    array_push($connectednodes, $deep);
-                }
-            }
-        }
-        array_push($totalArray, $connectednodes);
+    if($newStrings != $strings){
+        $newStrings = loop($newStrings);
     }
-    return $totalArray;
+    return $newStrings;
 }
+
